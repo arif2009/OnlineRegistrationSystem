5b45afae21d55b16beb6824aea9e6e37a53dad8a
Arif <arif.rahman2009@gmail.com>
2016-07-10 01:42:36 +0600

    Rearrange Project

--- OnlineRegistrationSystem/application/models/Adviser/dataInsertAM.php
+++
@@ -1,6 +0,0 @@
-<?php if(!defined('BASEPATH')) exit ('No direct script access allowed');
-class DataInsertAM extends CI_Model{
-    function __construct() {
-        parent::__construct();
-    }
-}
--- OnlineRegistrationSystem/application/views/commonCSS.php
+++
@@ -1,7 +0,0 @@
-<?=link_tag('images/others/duet.ico', 'icon', 'image/x-ico');?>
-<?=link_tag('images/others/duet.ico', 'shortcut icon', 'image/x-ico');?>
-<?=link_tag('css/960.css')?>
-<?=link_tag('css/reset.css')?>
-<?=link_tag('css/text.css')?>
-<?=link_tag('css/style.css')?>
-<?=link_tag('sdmenu/sdmenu.css')?> 
